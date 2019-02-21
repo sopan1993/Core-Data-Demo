31e1f48e569fd2216d4a8858e9fe4f9543e16ad6
Shirish Vispute <shirishvispute@Shirishs-MacBook-Pro.local>
2019-02-21 12:57:15 +0530

    First commit

---
+++ CoreDataNewDemoTests/CoreDataNewDemoTests.swift
@@ -0,0 +1,34 @@
+//
+//  CoreDataNewDemoTests.swift
+//  CoreDataNewDemoTests
+//
+//  Created by Shirish Vispute on 19/02/19.
+//  Copyright © 2019 Bitware Technologies. All rights reserved.
+//
+
+import XCTest
+@testable import CoreDataNewDemo
+
+class CoreDataNewDemoTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
